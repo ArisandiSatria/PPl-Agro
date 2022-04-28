bb121588336672a3fd017c813556b11ef4c9b64b
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2022-04-28 20:35:35 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,5 @@
 <?php
-define("HOST","localhost");
-define("USER","root");
-define("PASS","");
-define("DB","ppl");
-
-$conn = new mysqli("localhost","root","","ppl");
+$conn = new mysqli("localhost","root","","seed_up");
 
 if ($conn->connect_error) {
     die("Koneksi Gagal");
@@ -21,11 +16,8 @@ function query($query) {
     return $rows;
 }
 
-$sql = "CREATE TABLE IF NOT EXISTS laporanpencatatan (
-    id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tanggal VARCHAR(30) NOT NULL,
-	judulCatatan VARCHAR(50) NOT NULL,
-	isiCatatan VARCHAR(500) NOT NULL
-)";
-
-$conn->query($sql);
+function execute($query) {
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
