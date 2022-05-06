3003df1c4982e898bec29089b28c2fbf476e70b2
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2022-05-06 20:09:02 +0700

    Update file info cuaca
    - Penambahan navbar
    - Penyesuaian tampilan agar lebih nyaman dilihat

--- infoCuaca2.php
+++ infoCuaca2.php
@@ -1,8 +1,6 @@
 <?php 
-
-require_once("./db.php");
-
 session_start();
+require_once("./db.php");
 
 $query = "SELECT * FROM akun_pemilik";
 $stmt = $conn->query($query);
@@ -25,6 +23,10 @@ $hasil = $stmt->fetch_assoc();
             font-style: poppins, sans-serif;
             margin: 0;
             padding: 0;
+        }
+        body {
+            background-image: url(img/bgppl.jpeg);
+            background-size: cover;
         }
         nav {
             padding-top: 20px;
@@ -109,7 +111,6 @@ $hasil = $stmt->fetch_assoc();
             margin-left: 5px;
         }
 
-
         .maxValues{
             text-align: center;
             display: inline-block;
@@ -121,66 +122,77 @@ $hasil = $stmt->fetch_assoc();
             margin-left: 5px;
         }
     </style>
-<body>
+<body onload="DefaultScreen()">
     <div class="container mt-4">
-        <nav class="row ps-4">
-            <h3 class="col-2">SEEDS_UP</h3>
-            <ul class="col-5 d-flex justify-content-between">
+    <nav class="row ps-4">
+            <div class="col-1">
+                <img src="img/logoppl-removebg-preview.png" style="position:absolute;top:4%;height: 4rem; width: 4.5rem;">
+            </div>
+            <ul class="col-7 d-flex justify-content-between">
                 <li><a href="tampilanOwner2.php"><h6>Dashboard</h6></a></li>
-                <li><a href="laporanPencatatan2.php"><h6>Laporan Pencatatan</h6></a></li>
                 <li><a href="infoCuaca2.php"><h6>Info Cuaca</h6></a></li>
-                <li><a href="pemasaran2.php"><h6>Pemasaran</h6></a></li>
+                <li><a href="laporanPencatatan2.php"><h6>Laporan Pencatatan</h6></a></li>
+                <li><a href="pemasaran2.php"><h6>Produk</h6></a></li>
+                <li><a href="riwayatTransaksi.php"><h6> Riwayat Transaksi</h6></a></li>
             </ul>
-            <h5 class="col-3 offset-2 pt-2 d-flex justify-content-end">Halo, <?=$hasil["username"]?></h5>
+            <h5 class="col-2 offset-2 pt-2 d-flex justify-content-end">
+                <a href="profilOwner.php" style="text-decoration: none;color: #34364a;">
+                <i class="bi bi-person"></i>
+                <?php echo "Halo, " . $_SESSION['username_owner'] ."!". ""; ?>
+                </a>
+            </h5>
         </nav>
     </div>
     <div class="container mt-5">
         <h5 style="color: rgba(255, 117, 24, 1);">Lihat perkiraan cuaca 5 hari ke depan yuk!</h5>
         <h2 class="mb-3" style="font-weight: bold;">Cuaca</h2>
-        <div class="d-flex justify-content-between">
-            <h4 id="cityName">Perkiraan cuaca di kota Jember</h4>
-            <p id="inputContainer">Cari Kota : <input type="text" id="cityInput"><button type="button" onclick="GetInfo()">Cari</button></p>
+        <div class="row" id="inputContainer">
+            <div class="d-flex justify-content-between col">
+                <h4 id="cityName" class="">Perkiraan cuaca di kota Jember</h4>
+                <label class="form-label pt-2">Cari Kota:</label>
+            </div>
+            <div class="d-flex col-2">
+                <input type="text" id="cityInput" class="form-control">
+            </div>
+            <button onclick="GetInfo()" class="btn col-1" type="button" style="background-color: rgba(255, 117, 24, 1); color: white;">Cari</button>
         </div>
-        <div id = "weatherContainer">
-            <div id = "iconsContainer" class="d-flex justify-content-evenly">    
-                <div class = "icons">
-                    <p class="weather" id="day1"></p>
-                    <div class="image"><img src="img/dots.png" class="imgClass" id="img1"></div>
-                    <p class="minValues" id="day1Min">Loading...</p>
-                    <p class="maxValues" id="day1Max">Loading...</p>
-                </div>
-                <div class = "icons">
-                    <p class="weather" id="day2"></p>
-                    <div class="image"><img src="img/dots.png" class="imgClass" id="img2"></div>
-                    <p class="minValues" id="day2Min">Loading...</p>
-                    <p class="maxValues" id="day2Max">Loading...</p>
-                </div>
-                <div class = "icons">
-                    <p class="weather" id="day3"></p>
-                    <div class="image"><img src="img/dots.png" class="imgClass" id="img3"></div>
-                    <p class="minValues" id="day3Min">Loading...</p>
-                    <p class="maxValues" id="day3Max">Loading...</p>
-                </div>
-                <div class = "icons">
-                    <p class="weather" id="day4"></p>
-                    <div class="image"><img src="img/dots.png" class="imgClass" id="img4"></div>
-                    <p class="minValues" id="day4Min">Loading...</p>
-                    <p class="maxValues" id="day4Max">Loading...</p>
-                </div>
-                <div class = "icons">
-                    <p class="weather" id="day5"></p>
-                    <div class="image"><img src="img/dots.png" class="imgClass" id="img5"></div>
-                    <p class="minValues" id="day5Min">Loading...</p>
-                    <p class="maxValues" id="day5Max">Loading...</p>
-                </div>
+        <div class="d-flex justify-content-evenly mt-2">    
+            <div class = "icons">
+                <p class="weather" id="day1"></p>
+                <div class="image"><img src="img/dots.png" class="imgClass" id="img1"></div>
+                <p class="minValues" id="day1Min">Loading...</p>
+                <p class="maxValues" id="day1Max">Loading...</p>
+            </div>
+            <div class = "icons">
+                <p class="weather" id="day2"></p>
+                <div class="image"><img src="img/dots.png" class="imgClass" id="img2"></div>
+                <p class="minValues" id="day2Min">Loading...</p>
+                <p class="maxValues" id="day2Max">Loading...</p>
+            </div>
+            <div class = "icons">
+                <p class="weather" id="day3"></p>
+                <div class="image"><img src="img/dots.png" class="imgClass" id="img3"></div>
+                <p class="minValues" id="day3Min">Loading...</p>
+                <p class="maxValues" id="day3Max">Loading...</p>
+            </div>
+            <div class = "icons">
+                <p class="weather" id="day4"></p>
+                <div class="image"><img src="img/dots.png" class="imgClass" id="img4"></div>
+                <p class="minValues" id="day4Min">Loading...</p>
+                <p class="maxValues" id="day4Max">Loading...</p>
+            </div>
+            <div class = "icons">
+                <p class="weather" id="day5"></p>
+                <div class="image"><img src="img/dots.png" class="imgClass" id="img5"></div>
+                <p class="minValues" id="day5Min">Loading...</p>
+                <p class="maxValues" id="day5Max">Loading...</p>
             </div>
         </div>
-
-        <script src="script.js"></script>
     </div>
 </body>
-    
 
+
+<script src="script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
